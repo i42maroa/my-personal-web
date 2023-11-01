0792370cf6e40a57269276e795594d5f9fee63b4
Antonio Marín <amrodriguezjarote@gmail.com>
2023-11-01 13:28:35 +0100

    Add desktop svg

--- app/components/home/description/component.tsx
+++ app/components/home/description/component.tsx
@@ -1,6 +1,7 @@
 import { LINKS, PAGES } from '@/data/links.data'
 import styles from './Description.module.css'
 import { EngranajeSvg } from '../../shared/svg/Engranaje/Engranaje'
+import { DesktopSvg } from '../../shared/svg/Desktop/Desktop'
 
 export default function DescriptionComponent () {
   return (
@@ -9,15 +10,18 @@ export default function DescriptionComponent () {
         <h2>Diseño</h2>
         <p>Propongo soluciones llamativas siempre garantizando una buena experiencia de usuario. Lo que no entra por los ojos no funciona.</p>
       </div>
-      <div />
-      <div />
-      <div className={styles.textContainer}>
-        <h2>Ingeniería</h2>
-        <p>Un bonito diseño no es nada sin una buena maquinaria detrás. Todos los proyectos garantizan una buena optimización.</p>
+
+      <div className={styles.engranajeContainer}>
+        <DesktopSvg />
       </div>
 
       <div className={styles.engranajeContainer}>
         <EngranajeSvg />
+      </div>
+
+      <div className={styles.textContainer}>
+        <h2>Ingeniería</h2>
+        <p>Un bonito diseño no es nada sin una buena maquinaria detrás. Todos los proyectos garantizan una buena optimización.</p>
       </div>
 
     </section>
