27ffb40c433bfb533d767ccc13d0c56f95bf953d
Antonio Marín <amrodriguezjarote@gmail.com>
2023-11-03 13:33:19 +0100

    Ajust animation time and add data project

--- data/projects.data.tsx
+++ data/projects.data.tsx
@@ -1,8 +1,9 @@
 import { ProjectInterface } from '@/app/interface/project.interface'
 import { ENGLISH_TRAINNING_PROJECT } from './projects/english_training_project.data'
+import { COUNTRY_MANAGEMENT_APP_PROJECT, COUNTRY_MANAGEMENT_BACK_PROJECT } from './projects/country_management_project.data'
 
 export const PROJECTS_DATA: ProjectInterface[] = [
   ENGLISH_TRAINNING_PROJECT,
-  ENGLISH_TRAINNING_PROJECT,
-  ENGLISH_TRAINNING_PROJECT
+  COUNTRY_MANAGEMENT_BACK_PROJECT,
+  COUNTRY_MANAGEMENT_APP_PROJECT
 ]
--- data/technologies.data.tsx
+++ data/technologies.data.tsx
@@ -13,7 +13,6 @@ export const FRONT_TECHNOLOGY: TechnologyInterface[] = [
 
 export const BACK_TECHNOLOGY: TechnologyInterface[] = [
   TEC.SPRING,
-  TEC.JAVA,
   TEC.GRAPHQL,
   TEC.MONGO_DB,
   TEC.KAFKA
