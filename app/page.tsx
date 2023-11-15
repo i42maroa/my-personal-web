23248c8a627d3c02ab4eb86e119080e61e901ba4
Antonio Marín <amrodriguezjarote@gmail.com>
2023-11-15 17:01:50 +0100

    Add delemiter

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,21 @@ import ContactComponent from './components/home/contact/component'
 import DescriptionComponent from './components/home/description/component'
 import ThecnologiesComponent from './components/home/thecnologies/component'
 import ProjectsComponent from './components/home/projects/component'
+import { Delimiter } from './components/shared/delimiter/Delimiter'
 
 export default function Home () {
   return (
-    <><PresentationComponent /><DescriptionComponent /><ExperienceComponent /><ThecnologiesComponent /><ProjectsComponent /><ContactComponent /></>
+    <><PresentationComponent />
+      <Delimiter />
+      <DescriptionComponent />
+      <Delimiter />
+      <ExperienceComponent />
+      <Delimiter />
+      <ThecnologiesComponent />
+      <Delimiter />
+      <ProjectsComponent />
+      <Delimiter />
+      <ContactComponent />
+    </>
   )
 }
