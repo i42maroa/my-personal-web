45d5e3d4e463f950b55516f6eb02cdd52f93b0c3
Antonio Marín <amrodriguezjarote@gmail.com>
2023-11-21 20:15:27 +0100

    Experence refactor

--- app/components/shared/cards/experience/Experience.tsx
+++ app/components/shared/cards/experience/Experience.tsx
@@ -13,13 +13,14 @@ export function Experience ({ experience }: { experience: ExperienceInterface })
         <h3 className={styles.title}> {experience.title}</h3>
         <h4 className={styles.subtitle}>{experience.subtitle}</h4>
       </div>
-      <div />
+      <div className={styles.complementaryDescriptionContainer} />
       <div className={styles.descriptionContainer}>
         {experience.descriptions.length > 0 &&
           experience.descriptions.map((e) => (
             <div className={styles.descriptionContainerUnity} key={e.title}>
               <div className={styles.titleDescriptionContainer}>
                 <h4 className={styles.titleDescription}>{e.title}</h4>
+                <div className={styles.separateLine} />
                 <p className={styles.description}>{e.description}</p>
               </div>
               <div className={styles.contentTechnologies}>
@@ -29,6 +30,7 @@ export function Experience ({ experience }: { experience: ExperienceInterface })
                   </div>
                 ))}
               </div>
+
             </div>
           ))}
       </div>
