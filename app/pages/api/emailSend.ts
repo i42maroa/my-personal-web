f2a043ec16e84b07f0d71113297163c2c857cb00
Antonio Marín <amrodriguezjarote@gmail.com>
2023-11-22 21:11:43 +0100

    Fix lint issue

--- app/pages/api/emailSend.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import emailjs from 'emailjs-com'
-
-export default async function handler (
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const { name, email, message } = req.body
-}
