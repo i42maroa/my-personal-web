6643a67e0ed672ec7d86fce653590a11297487e2
Antonio Marín <amrodriguezjarote@gmail.com>
2023-11-22 21:07:19 +0100

    Add contact form

--- app/components/home/contact/component.tsx
+++ app/components/home/contact/component.tsx
@@ -1,10 +1,9 @@
+'use client'
+
 import { LINKS, PAGES } from '@/data/links.data'
 import styles from './Contact.module.css'
 import { WorkCoffee } from '../../shared/svg/Work&Coffee/WorkCoffee'
-import { LinkedinSvg } from '../../shared/svg/Buttons/Linkedin'
-import { GmailSvg } from '../../shared/svg/Buttons/Gmail'
-import { InstagramSvg } from '../../shared/svg/Buttons/Instagram'
-import Link from 'next/link'
+import ContactForm from '../../shared/form/ContactForm'
 
 export default function ContactComponent () {
   return (
@@ -15,10 +14,10 @@ export default function ContactComponent () {
           <WorkCoffee />
         </div>
         <div className={styles.textContainer}>
-          <h2>Contactame</h2>
+          <h2>Contáctame</h2>
           <div className={styles.pageContent}>
             <p className={styles.text}>No hay nada que con un café no se pueda conseguir.</p>
-            <div className={styles.buttonContainer}>
+            {/* <div className={styles.buttonContainer}>
               <Link href='https://www.linkedin.com/in/antonio-mar%C3%ADn-rodr%C3%ADguez-07681b1bb/' className={styles.button}>
                 <LinkedinSvg />
               </Link>
@@ -28,7 +27,8 @@ export default function ContactComponent () {
               <Link href='/#contact' className={styles.button}>
                 <InstagramSvg />
               </Link>
-            </div>
+            </div> */}
+            <ContactForm />
           </div>
 
         </div>
---
+++ app/components/shared/form/ContactForm.tsx
@@ -0,0 +1,34 @@
+import emailjs from '@emailjs/browser'
+import { useState } from 'react'
+import styles from './ContactForm.module.css'
+
+export default function ContactForm () {
+  const [formData, setFormData] = useState({ user_name: '', user_email: '', user_message: '' })
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const id = 'service_l8afvph'
+    const templateId = 'template_pb7xl9l'
+    const publicKey = 'PTbPZyQDA6HdARA-H'
+    event.preventDefault()
+
+    emailjs.sendForm(id, templateId, event.target, publicKey)
+      .then(response => console.log(response))
+      .catch(error => console.log(error))
+  }
+
+  return (
+    <form className={styles.formContainer} onSubmit={handleSubmit}>
+      <input className={styles.input} type='text' id='name' name='user_name' placeholder='Nombre contacto' value={formData.user_name} onChange={handleChange} />
+      <input className={styles.input} type='email' id='email' name='user_email' placeholder='Email' value={formData.user_email} onChange={handleChange} />
+      <textarea className={styles.textarea} id='message' name='user_message' placeholder='Mensaje' value={formData.user_message} onChange={handleChange} />
+
+      <button className={styles.formButton} type='submit'>Submit</button>
+    </form>
+  )
+}
