2ba825a854ba4268eaa4ba1f2516c1f7631b5c2c
Antonio Marín <amrodriguezjarote@gmail.com>
2024-01-19 21:23:19 +0100

    Change machine colors

--- hook/changeContext.tsx
+++ hook/changeContext.tsx
@@ -6,9 +6,7 @@ import { playCoffeeMachineSound } from './playCoffeeMachineSound'
 export interface CONTEXT_STATE {num:number, show:boolean};
 
 export default function changeContextState (state:CONTEXT_STATE, newState: CONTEXT_STATE) :CONTEXT_STATE {
-  console.log(state, 'state', newState, 'newState')
   if (state.num !== newState.num) {
-    console.log('enter')
     const type = STATE_TOGGLE[newState.num].color
     const lastType = STATE_TOGGLE[state.num].color
 
